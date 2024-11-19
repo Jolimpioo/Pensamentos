e4ba1d61ec0fae703f7d6b24e81188659615fd88
Jolimpioo <jolimpiomeloj@gmail.com>
2024-11-19 11:04:05 -0300

    Add funcionalidade de Edicao e Atualizacao

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -21,7 +21,13 @@ export default class ToughtController {
     }
 
     const toughts = user.Thoughts.map((result) => result.dataValues);
-    res.render("toughts/dashboard", { toughts });
+
+    let emptyToughts = false;
+    if (toughts.length === 0) {
+      emptyToughts = true;
+    }
+
+    res.render("toughts/dashboard", { toughts, emptyToughts });
   }
 
   static createTought(req, res) {
@@ -56,6 +62,34 @@ export default class ToughtController {
       await Tought.destroy({ where: { id: id, UserId: UserId } });
 
       req.flash("message", "Pensamento removido com sucesso!");
+      req.session.save(() => {
+        res.redirect("/toughts/dashboard");
+      });
+    } catch (error) {
+      console.log("Aconteceu um erro: " + error);
+    }
+  }
+
+  static async updateTought(req, res) {
+    const id = req.params.id;
+
+    const tought = await Tought.findOne({ where: { id: id }, raw: true });
+
+    res.render("toughts/edit", { tought });
+  }
+
+  static async updateToughtSave(req, res) {
+    const id = req.body.id;
+
+    const tought = {
+      title: req.body.title,
+    };
+
+    try {
+      // Atualiza o pensamento no banco de dados
+      await Tought.update(tought, { where: { id: id } });
+      req.flash("message", "Pensamento atualizado com sucesso!");
+
       req.session.save(() => {
         res.redirect("/toughts/dashboard");
       });
