2c016a82c29d593bf8aa8d3a47a4dec99c60a11c
Jolimpioo <jolimpiomeloj@gmail.com>
2024-11-19 16:28:22 -0300

    add funcionalidade de busca

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -1,15 +1,32 @@
 import Tought from "../models/Tought.js";
 import User from "../models/User.js";
+import { Op } from "sequelize";
 
 export default class ToughtController {
   static async showToughts(req, res) {
+    let search = "";
+    if (req.query.search) {
+      search = req.query.search;
+    }
+
     const taughtsData = Tought.findAll({
       include: User,
+      where: {
+        title: { [Op.like]: `%${search}%` },
+      },
     });
 
-    const toughts = (await taughtsData).map((result) => result.get({plain: true}));
+    const toughts = (await taughtsData).map((result) =>
+      result.get({ plain: true })
+    );
 
-    res.render("toughts/home", { toughts });
+    let toughtsQty = toughts.length;
+
+    if (toughtsQty === 0) {
+      toughtsQty = false;
+    }
+
+    res.render("toughts/home", { toughts, search, toughtsQty });
   }
 
   static async dashboard(req, res) {
